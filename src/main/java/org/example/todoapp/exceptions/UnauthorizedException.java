ecde4475072b39a591f495a3ebe923714ffcc199
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2025-03-29 00:25:16 +1000

    added ItemGetDto, ItemMapper, upd UnauthorizedException

--- src/main/java/org/example/todoapp/exceptions/UnauthorizedException.java
+++ src/main/java/org/example/todoapp/exceptions/UnauthorizedException.java
@@ -3,8 +3,8 @@ package org.example.todoapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class BadCredentialsException extends ResponseStatusException {
-    public BadCredentialsException(String message) {
+public class UnauthorizedException extends ResponseStatusException {
+    public UnauthorizedException(String message) {
         super(HttpStatus.UNAUTHORIZED, message);
     }
 }
