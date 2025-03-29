d3342e4f905e25d2e0108ce25db0dd9a23c72ba9
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2025-03-30 00:18:16 +1000

    init update in TodoListController

--- src/main/java/org/example/todoapp/services/impl/TodoListServiceImpl.java
+++ src/main/java/org/example/todoapp/services/impl/TodoListServiceImpl.java
@@ -3,6 +3,8 @@ package org.example.todoapp.services.impl;
 import java.util.*;
 
 import lombok.*;
+import org.example.todoapp.exceptions.*;
+import org.example.todoapp.exceptions.BadRequestException;
 import org.example.todoapp.models.*;
 import org.example.todoapp.repositories.*;
 import org.example.todoapp.services.TodoListService;
@@ -15,9 +17,15 @@ public class TodoListServiceImpl implements TodoListService {
 
     @Override
     public Long save(TodoList todoList) {
-        return todoListRepository
-            .save(todoList)
-            .getId();
+        var exists = todoListRepository.findTodoListByIdAndUserId(todoList.getId(),
+            todoList.getUser().getId());
+
+        if (exists.isEmpty()) {
+            return todoListRepository
+                .save(todoList)
+                .getId();
+        }
+        throw new ConflictException("Todo list already exists");
     }
 
     @Override
@@ -27,7 +35,21 @@ public class TodoListServiceImpl implements TodoListService {
 
     @Override
     public TodoList findByIdAndUserId(Long id, Long userId) {
-        return todoListRepository.findTodoListByIdAndUserId(id, userId);
+        return todoListRepository.findTodoListByIdAndUserId(id, userId)
+            .orElseThrow(() -> new NotFoundException("Not found todo list"));
+    }
+
+    @Override
+    public void update(TodoList todoList, Long userId) {
+        var existingTodoList = todoListRepository.findById(todoList.getId())
+            .orElseThrow(() -> new NotFoundException("Todo list not found"));
+        if (!existingTodoList.getUser().getId().equals(userId)) {
+            throw new BadRequestException("User id mismatch");
+        }
+
+        existingTodoList.setTitle(todoList.getTitle());
+        existingTodoList.setDescription(todoList.getDescription());
+        todoListRepository.save(existingTodoList);
     }
 
 }
