d644d8ae84640fb6606b8c985f272d85d41544b0
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2025-03-29 23:48:47 +1000

    init save in TodoListServiceImpl

--- src/main/java/org/example/todoapp/repositories/TodoListRepository.java
+++ src/main/java/org/example/todoapp/repositories/TodoListRepository.java
@@ -1,8 +1,12 @@
 package org.example.todoapp.repositories;
 
+import java.util.*;
+
 import org.example.todoapp.models.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
-
+    List<TodoList> findByUserId(Long userId);
 }
