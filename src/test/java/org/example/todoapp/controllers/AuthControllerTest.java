748d7a3f92b4d9b2911a7dbce85e4fe1cd14a8b4
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2025-03-30 22:40:45 +1000

    create tests signIn_ShouldReturnJwtToken, signIn_ShouldReturnNotFound for AuthController

--- src/test/java/org/example/todoapp/controllers/AuthControllerTest.java
+++ src/test/java/org/example/todoapp/controllers/AuthControllerTest.java
@@ -3,11 +3,13 @@ package org.example.todoapp.controllers;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.*;
 import org.example.todoapp.dto.*;
+import org.example.todoapp.exceptions.BadCredentialsException;
 import org.example.todoapp.services.*;
 import org.example.todoapp.utils.*;
 import org.mockito.*;
 import org.springframework.http.*;
 import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.setup.*;
 
@@ -63,8 +65,38 @@ public class AuthControllerTest {
     }
 
     @Test
-    void signIn() throws Exception {
+    void signIn_ShouldReturnJwtToken() throws Exception {
+        var loginDto = new UserLoginDto(
+            "test@example.com",
+            "1324"
+        );
+        var userDetails = mock(UserDetails.class);
+        var expectedToken = "test.jwt.token";
 
+        when(userService.loadUserByUsername(loginDto.getEmail())).thenReturn(userDetails);
+        when(jwtTokenUtils.generateToken(userDetails)).thenReturn(expectedToken);
+
+        ResponseEntity<?> response = authController.signIn(loginDto);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertInstanceOf(JwtResponse.class, response.getBody());
+        assertEquals(expectedToken, ((JwtResponse) response.getBody()).getToken());
+        verify(userService, times(1)).loadUserByUsername(loginDto.getEmail());
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtTokenUtils).generateToken(userDetails);
+    }
+
+    @Test
+    void signIn_ShouldReturnNotFound() throws Exception {
+        var loginDto = new UserLoginDto(
+            "test@example.com",
+            "1324"
+        );
+
+        when(authenticationManager.authenticate(any()))
+            .thenThrow(new BadCredentialsException("Bad Credentials"));
+
+        assertThrows(BadCredentialsException.class, () -> authController.signIn(loginDto));
     }
 
 }
