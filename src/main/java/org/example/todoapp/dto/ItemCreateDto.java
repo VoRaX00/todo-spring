1b5c0c8ed7c5873e692f98ee1fae51db6fc39696
Кержаков Никита <nkerzhakov@atbplugin.tech>
2025-04-05 00:21:07 +1000

    The TodoListController is fully covered with tests

--- src/main/java/org/example/todoapp/dto/ItemCreateDto.java
+++ src/main/java/org/example/todoapp/dto/ItemCreateDto.java
@@ -3,6 +3,7 @@ package org.example.todoapp.dto;
 import lombok.*;
 
 @Data
+@Builder
 public class ItemCreateDto {
     private String title;
     private String description;
--- src/main/java/org/example/todoapp/dto/TodoListCreateDto.java
+++ src/main/java/org/example/todoapp/dto/TodoListCreateDto.java
@@ -3,6 +3,7 @@ package org.example.todoapp.dto;
 import lombok.*;
 
 @Data
+@Builder
 public class TodoListCreateDto {
     private String title;
     private String description;
